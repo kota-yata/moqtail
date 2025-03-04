41927f90c5018ac78a8872b136b5f89a0359db85
kota-yata <kota@yatagai.com>
2025-03-04 20:01:54 +0900

    add serverSetup

---
+++ src/messages/serverSetup.ts
@@ -0,0 +1,18 @@
+import { CONTROL_MESSAGE } from "../constants";
+import { deserializeParams, Parameter, serializeParams } from "../parameter";
+import { concatBuffer, numberToVarInt, varIntToNumber } from "../utils/bytes";
+
+export const serializeServerSetup = (props: { version: number, params: Parameter[] }) => {
+  const messageType = numberToVarInt(CONTROL_MESSAGE.SERVER_SETUP);
+  const selectedVersion = numberToVarInt(props.version);
+  const params = serializeParams(props.params);
+  const length = numberToVarInt(concatBuffer([selectedVersion, params]).length);
+  return concatBuffer([messageType, length, selectedVersion, params]);
+}
+
+export const deserizlizeServerSetup = async (controlReader: ReadableStream) => {
+  await varIntToNumber(controlReader); // length
+  const selectedVersion = await varIntToNumber(controlReader);
+  const parameters = await deserializeParams(CONTROL_MESSAGE.SERVER_SETUP, controlReader);
+  return { selectedVersion, parameters };
+}
