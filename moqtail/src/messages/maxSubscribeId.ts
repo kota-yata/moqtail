6222d503a758bec42d0c9f7924c8b681e608c789
kota-yata <kota@yatagai.com>
2025-05-30 21:16:59 +0900

    update control messages' code

--- moqtail/src/messages/maxSubscribeId.ts
+++ moqtail/src/messages/maxSubscribeId.ts
@@ -2,7 +2,7 @@ import { CONTROL_MESSAGE } from "../constants";
 import { concatBuffer, serializeQuicVarInt, deserializeQuicVarInt } from "../utils/bytes";
 
 export const serializeMaxSubscribeId = (props: { subscribeId: number }) => {
-  const messageType = serializeQuicVarInt(CONTROL_MESSAGE.MAX_SUBSCRIBE_ID);
+  const messageType = serializeQuicVarInt(CONTROL_MESSAGE.MAX_REQUEST_ID);
   const subscribeIdBytes = serializeQuicVarInt(props.subscribeId);
   const length = serializeQuicVarInt(subscribeIdBytes.byteLength);
   return concatBuffer([messageType, length, subscribeIdBytes]);
