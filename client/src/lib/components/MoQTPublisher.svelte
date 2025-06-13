d6cea786c4c9d9043b4daf2b5cdf911984556f60
kota-yata <kota@yatagai.com>
2025-06-13 23:25:45 +0900

    remove duplicated at

--- client/src/lib/components/MoQTPublisher.svelte
+++ client/src/lib/components/MoQTPublisher.svelte
@@ -113,7 +113,6 @@
     const vt = stream.getVideoTracks()[0];
     Mogger.info(`Streaming ${vt.label}`);
     publisher.startStream({ track: videoTrack, mediaTrack: vt });
-    const at = stream.getAudioTracks()[0];
     Mogger.info(`Streaming ${at.label}`);
     publisher.startStream({ track: audioTrack, mediaTrack: at });
   };
