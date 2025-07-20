aeed56d6e2370a21907160783b1b97ff85a6ce5d
kota-yata <kota@yatagai.com>
2025-07-20 19:13:40 +0900

    Remove unnecessary logging

--- moqtail-core/src/dataStreams/datagram.ts
+++ moqtail-core/src/dataStreams/datagram.ts
@@ -17,7 +17,6 @@ export const serializeDatagram = (props: Datagram) => {
   if (props.type === DATAGRAM_TYPE.WITH_EXTENSION) {
     extensionHeaderBytes = serializeExtensionHeaders(props.extensionHeaders);
   }
-  console.log(`Serialized Datagram: Type=${typeBytes}, TrackAlias=${trackAliasBytes}, GroupId=${groupIdBytes}, ObjectId=${objectIdBytes}, PublisherPriority=${publisherPriorityBytes}, ExtensionHeaders=${extensionHeaderBytes}`);
   const datagram = concatUint8Arrays([typeBytes, trackAliasBytes, groupIdBytes, objectIdBytes, publisherPriorityBytes, extensionHeaderBytes, props.payload]);
   return datagram;
 }
@@ -29,14 +28,12 @@ export const deserializeDatagramHeader = async (readableStream: ReadableStream):
   ret.groupId = await deserializeQuicVarInt(readableStream);
   ret.objectId = await deserializeQuicVarInt(readableStream);
   ret.publisherPriority = await getUint8(readableStream)
-  console.log(`Deserialized Datagram Header: Type=${ret.type}, TrackAlias=${ret.trackAlias}, GroupId=${ret.groupId}, ObjectId=${ret.objectId}, PublisherPriority=${ret.publisherPriority}`);
   ret.extensionHeaders = [];
   if (ret.type === DATAGRAM_TYPE.WITHOUT_EXTENSION) return ret;
   let extensionHeadersLength = await deserializeQuicVarInt(readableStream);
   ret.extensionHeaders = [];
   while (extensionHeadersLength > 0) {
     const v = await deserializeExtensionHeader(readableStream);
-    console.log(`Deserialized Extension Header: ${v.value.type} - ${v.value.value}`);
     ret.extensionHeaders.push(v.value);
     extensionHeadersLength -= v.byteLength;
   }
